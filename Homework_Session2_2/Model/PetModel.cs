579fa0f321d092e39e3d9e7cc207cc0aa827b2fa
josepherickreyes <josepherick.reyes@softvision.com>
2023-03-13 20:16:56 +0800

    Add project files.

---
+++ Homework_Session2_2/Model/PetModel.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Session2_2
+{
+    public class PetModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("category")]
+        public PetCategory Category { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("photoUrls")]
+        public string[] PhotoUrls { get; set; }
+
+        [JsonProperty("tags")]
+        public PetCategory[] Tags { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+    public class PetCategory
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Homework_Session2_2/PostPetMethod.cs
@@ -0,0 +1,85 @@
+
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+[assembly: Parallelize(Workers = 10, Scope = ExecutionScope.MethodLevel)]
+
+
+namespace HomeWork_Session2_2
+{
+    [TestClass]
+    public class PostPetMethod
+    {
+        private static RestClient restClient;
+
+        private static readonly string BaseURL = "https://petstore.swagger.io/v2/";
+
+        private static readonly string PetEndpoint = "pet";
+
+        private static string GetURL(string endpoint) => $"{BaseURL}{endpoint}";
+
+        private static Uri GetURI(string endpoint) => new Uri(GetURL(endpoint));
+
+        private readonly List<PetModel> cleanUpList = new List<PetModel>();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            restClient = new RestClient();
+        }
+
+        [TestCleanup]
+        public async Task TestCleanUp()
+        {
+            foreach (var data in cleanUpList)
+            {
+                var restDeleteRequest = new RestRequest(GetURL($"{PetEndpoint}/{data.Id}"));
+                var restDeleteResponse = await restClient.DeleteAsync(restDeleteRequest);
+
+            }
+        }
+
+
+        [TestMethod]
+        public async Task PostProject()
+        {
+            #region CreateNewPet
+            //Create New Pet
+            var newPet = new PetModel()
+            {
+                Id = 0,
+                Name = "Mr.Pickles",
+                PhotoUrls = new string[] { "photo " },
+                Tags = new PetCategory[] { new PetCategory { Name = "Tags" } },
+                Status = "available"
+            };
+
+            // Send Post Request
+            var temp = GetURI(PetEndpoint);
+            var postRestRequest = new RestRequest(GetURI(PetEndpoint)).AddJsonBody(newPet);
+            var restResponse = await restClient.ExecutePostAsync(postRestRequest);
+            #endregion
+
+            // Verify Post Request Status Code
+            Assert.AreEqual(HttpStatusCode.OK, restResponse.StatusCode, "Status Code is not equal to 200");
+
+            // Send Get Request
+            var getRestRequest = new RestRequest(GetURI($"{PetEndpoint}/{newPet.Id}"));
+            var getRestResponse = await restClient.ExecuteGetAsync<PetModel>(getRestRequest);
+
+            Assert.AreEqual(HttpStatusCode.OK, getRestResponse.StatusCode, "Status Code is not equal to 200");
+            Assert.AreEqual(newPet.Name, getRestResponse.Data?.Name, "Name did not Match");
+            Assert.AreEqual(newPet.Category.Name, getRestResponse.Data?.Category.Name, "Category did not Match");
+            Assert.AreEqual(newPet.PhotoUrls[0], getRestResponse.Data?.PhotoUrls[0], "PhotoUrls not found");
+            Assert.AreEqual(newPet.Tags[0].Name, getRestResponse.Data?.Tags[0].Name, "tags not found");
+            Assert.AreEqual(newPet.Status, getRestResponse.Data?.Status, "Status Code is not equal to 200");
+
+            // CleanUp
+            cleanUpList.Add(newPet);
+        }
+    }
+}
